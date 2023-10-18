22b2de791d6d95b086c09d8e24948f9670f33f2d
Kanomace <3065566279@qq.com>
2023-10-18 12:56:37 +0800

    update

--- caculation/main.py
+++ caculation/main.py
@@ -53,14 +53,14 @@ def power():
 window = tk.Tk()
 window.title("Calculator")
 
-# 创建一个Frame来容纳计算器的部件
+# Create a Frame to hold the calculator components
 frame = tk.Frame(window, padx=10, pady=10)
 frame.pack()
 
 entry = tk.Entry(frame, width=30, font=("Arial", 14))
 entry.grid(row=0, column=0, columnspan=4, pady=10)
 
-# 数字按钮
+# Number buttons
 buttons = [
     "7", "8", "9", "/",
     "4", "5", "6", "*",
@@ -78,7 +78,7 @@ for button in buttons:
         col = 0
         row += 1
 
-# 三角函数按钮
+# Trigonometric function buttons
 sin_btn = tk.Button(frame, text="sin", width=5, font=("Arial", 12), command=sin)
 sin_btn.grid(row=2, column=4, padx=5, pady=5)
 
@@ -88,15 +88,15 @@ cos_btn.grid(row=3, column=4, padx=5, pady=5)
 tan_btn = tk.Button(frame, text="tan", width=5, font=("Arial", 12), command=tan)
 tan_btn.grid(row=4, column=4, padx=5, pady=5)
 
-# 指数运算按钮
+# Exponentiation button
 power_btn = tk.Button(frame, text="^", width=5, font=("Arial", 12), command=power)
 power_btn.grid(row=5, column=4, padx=5, pady=5)
 
-# 计算按钮
+# Calculate button
 calc_btn = tk.Button(frame, text="=", width=12, font=("Arial", 12), command=calculate)
 calc_btn.grid(row=5, column=2, columnspan=2, padx=5, pady=5)
 
-# 清除按钮
+# Clear button
 clear_btn = tk.Button(frame, text="C", width=12, font=("Arial", 12), command=lambda: entry.delete(0, tk.END))
 clear_btn.grid(row=5, column=0, columnspan=2, padx=5, pady=5)
 
